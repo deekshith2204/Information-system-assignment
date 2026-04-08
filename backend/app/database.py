80373d1c93de656add7fe4cddc64928207e120c0
deekshith2204 <deekshiththadvai2204@gmail.com>
2026-04-08 17:55:22 +0100

    updated database.py to handle error using my friend

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,10 @@
+import os
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./babla_cars.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 

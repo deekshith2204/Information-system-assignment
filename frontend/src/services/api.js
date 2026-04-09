bbfd6cd342658af31ec4d494fb9b250029a34f47
deekshith2204 <deekshiththadvai2204@gmail.com>
2026-04-09 17:09:55 +0100

    changed the baseurl from local to azure backend url to upload in cloud

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://127.0.0.1:8000" });
+const API = axios.create({ baseURL: "https://rideshare-api-dmanbedkcvhbfqbh.italynorth-01.azurewebsites.net" });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
